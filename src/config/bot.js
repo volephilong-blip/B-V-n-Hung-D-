2fe180d73caa761a43049348a08cc9ca4c296421
volephilong-blip <volephilong@gmail.com>
2026-06-15 11:51:39 +0700

    bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "t là nữ hoàng nè mấy con đứng đường",
         // Activity type number (0 = Playing).
         type: 0, 
       },
